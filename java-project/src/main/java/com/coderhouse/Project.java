142fd1feb1e20b10d6629d57984df072eb5157f8
jpdalmolin <dalmolinjuanpablo@gmail.com>
2024-10-05 19:48:31 -0300

    dede

---
+++ java-project/src/main/java/com/coderhouse/Project.java
@@ -0,0 +1,12 @@
+package com.coderhouse;
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte variableA = 10;
+		byte variableB = 10;
+		System.out.println("result"+(variableA+variableB));
+	}
+
+}
